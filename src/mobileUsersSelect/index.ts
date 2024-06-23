39ec2843a5f4cfbf7f88409208cf2e6e7278459a
saeed mohammdiha <saeed1440124@gmail.com>
2024-06-23 13:29:38 +0330

    fix bugs

--- src/mobileUsersSelect/index.ts
+++ src/mobileUsersSelect/index.ts
@@ -15,6 +15,16 @@ export function addOptionsToMobileUsersSelect() {
     { id: "8", value: "8", text: "8" },
     { id: "9", value: "9", text: "9" },
     { id: "10", value: "10", text: "10" },
+    { id: "11", value: "11", text: "11" },
+    { id: "12", value: "12", text: "12" },
+    { id: "13", value: "13", text: "13" },
+    { id: "14", value: "14", text: "14" },
+    { id: "15", value: "15", text: "15" },
+    { id: "16", value: "16", text: "16" },
+    { id: "17", value: "17", text: "17" },
+    { id: "18", value: "18", text: "18" },
+    { id: "19", value: "19", text: "19" },
+    { id: "20", value: "20", text: "20" },
   ];
 
   const select = document.getElementById(
@@ -34,7 +44,7 @@ export function addOptionsToMobileUsersSelect() {
 }
 
 export function mobileUsersSelectOnChangeHandler(event: Event) {
-  const price = 60000000;
+  const price = 30000000;
   const value = (event.target as HTMLInputElement).value;
   setSelectPrice("mobileUserPrice", Number(value) * price);
   calculatePrice();
