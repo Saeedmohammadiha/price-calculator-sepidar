41ceced0143d5c23bd75c1cae158a5840a79b0a9
s.mohammadiha <s.mohammadiha@asanpardakht.ir>
2025-06-14 00:57:47 +0330

    feat: read the file and init

--- src/main.ts
+++ src/main.ts
@@ -4,23 +4,60 @@ import { addOptionsToMobileUsersSelect } from "./mobileUsersSelect";
 import { calculatePrice } from "./PriceCalculator";
 import { selectSystem } from "./selectSystem";
 import { addOptionsToSharedCompaniesSelect } from "./sharedCompaniesSelect";
+import { initData } from "./state/initData";
 import { subSystemsList } from "./state/subSystemsList";
 import { systemsList } from "./state/systemsList";
 import "./styles.css";
 import { addOptionsToUsersCountSelect } from "./usersCountSelect";
 
+declare global {
+  interface Window {
+    ourData: {
+      systemsList: {
+        id: `sys_${number}`;
+        name: string;
+        price: string;
+      }[];
+      subSystemsList: {
+        id: `subSys_${number}`;
+        name: string;
+        price: string;
+      }[];
+      lockSelectPerPrice: number;
+      mobileUsersSelectPrice: number;
+      usersCountSelectPrice: number;
+      htmlDataOnePc: HtmlData;
+      htmlDataMorePc: HtmlData;
+      htmlDataInstallService: HtmlData;
+      htmlDataHourlyService: HtmlData;
+    };
+  }
+}
+
+type HtmlData = {
+  lable: string;
+  price: string;
+};
+
 document.addEventListener("DOMContentLoaded", () => {
+  fetch("./data.json")
+    .then((res) => {
+      return res.json();
+    })
+    .then((ourData) => {
+      window.ourData = ourData;
+      initData();
+      renderSystemsList(systemsList);
+      renderSubSystemsList(subSystemsList);
+      calculatePrice();
 
-
-
-
-  renderSystemsList(systemsList);
-  renderSubSystemsList(subSystemsList);
-  calculatePrice();
-
-  selectSystem();
-  addOptionsToLockSelect();
-  addOptionsToUsersCountSelect();
-  addOptionsToSharedCompaniesSelect();
-  addOptionsToMobileUsersSelect();
+      selectSystem();
+      addOptionsToLockSelect();
+      addOptionsToUsersCountSelect();
+      addOptionsToSharedCompaniesSelect();
+      addOptionsToMobileUsersSelect();
+    })
+    .catch((er) => {
+      console.log(er);
+    });
 });
